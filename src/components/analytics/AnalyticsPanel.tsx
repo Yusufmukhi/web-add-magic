8b2af1b80898852c8121a792e1e75f4a3f845d57
Yusufmukhi <yusufmukhi1@gmail.com>
2026-05-23 15:25:31 +0530

    Update AnalyticsPanel.tsx

--- src/components/analytics/AnalyticsPanel.tsx
+++ src/components/analytics/AnalyticsPanel.tsx
@@ -2,6 +2,8 @@ import { SectorAllocation } from "./SectorAllocation";
 import { SectorEditor } from "./SectorEditor";
 import { PortfolioVsNiftyChart } from "./PortfolioVsNiftyChart";
 import { ReturnsComparison } from "./ReturnsComparison";
+import { HoldingsHeatmap } from "./HoldingsHeatmap";
+import { CompareChart } from "./CompareChart";
 import type { Holding } from "@/types/portfolio.types";
 import type { QuoteResult } from "@/hooks/useStockQuote";
 
@@ -15,6 +17,8 @@ export function AnalyticsPanel({ portfolio, results }: Props) {
     <div className="space-y-6">
       <PortfolioVsNiftyChart portfolio={portfolio} />
       <ReturnsComparison portfolio={portfolio} />
+      <HoldingsHeatmap portfolio={portfolio} results={results} />
+      <CompareChart />
       <div className="grid gap-6 lg:grid-cols-2">
         <SectorAllocation results={results} />
         <SectorEditor results={results} />
