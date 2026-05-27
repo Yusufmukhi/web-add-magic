6e6a69fdc8e9325610ffb170af636cb372666e1e
Yusufmukhi <yusufmukhi1@gmail.com>
2026-05-27 15:01:50 +0530

    Update BuyStockModal.tsx

--- src/components/modals/BuyStockModal.tsx
+++ src/components/modals/BuyStockModal.tsx
@@ -230,7 +230,7 @@ export function BuyStockModal({ open, onClose, cashBalance, onConfirm }: Props) 
             {submitting ? <><Loader2 className="mr-1 h-4 w-4 animate-spin" /> Buying…</> : "Buy"}
           </Button>
         </div>
-        </div>
+        
     </MobileSheet>
   );
 }
