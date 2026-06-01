ab397749d827617732292e1c7bbc888abff44423
Yusufmukhi <yusufmukhi1@gmail.com>
2026-06-01 13:51:58 +0530

    Delete src/routes/finance.tsx

--- src/routes/finance.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { FinanceTrackerPage } from "@/pages/finance/FinanceTrackerPage";
-
-export const Route = createFileRoute("/finance")({
-  component: FinanceTrackerPage,
-});
