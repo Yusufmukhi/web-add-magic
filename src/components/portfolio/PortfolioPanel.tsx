f37749409f7712e6b94f1eb244333fc0f97a9c08
Yusufmukhi <yusufmukhi1@gmail.com>
2026-06-04 11:15:09 +0530

    Update PortfolioPanel.tsx

--- src/components/portfolio/PortfolioPanel.tsx
+++ src/components/portfolio/PortfolioPanel.tsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useStockQuotes } from "@/hooks/useStockQuote";
 import type { Holding, HoldingRow, Transaction } from "@/types/portfolio.types";
 import { PortfolioStats } from "./PortfolioStats";
-import { PortfolioMetricStrip } from "./PortfolioMetricStrip";
 import { HoldingsTable } from "./HoldingsTable";
 import { AllocationDonut } from "./AllocationDonut";
 import { PortfolioActions } from "./PortfolioActions";
@@ -315,15 +314,7 @@ export function PortfolioPanel({
 
   return (
     <div className="space-y-6">
-      {/* Mobile metric chips strip */}
-      <div className="md:hidden">
-        <PortfolioMetricStrip
-          invested={invested}
-          current={current}
-          cashBalance={cashBalance}
-          cagr={cagr}
-        />
-      </div>
+
       <PortfolioActions
         onAddFunds={onAddFunds} onWithdraw={onWithdraw} onBuy={onBuy}
         onSell={() => onSell()} onExportExcel={handleExportExcel}
